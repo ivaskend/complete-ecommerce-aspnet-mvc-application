8d694771d1d53605251e6037f7370676fd87160c
ivaskend <ivaskenderova98@gmail.com>
2023-03-03 00:25:16 +0100

    added models

---
+++ Vinylsque/Data/VinylGenre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vinylsque.Data
+{
+    public enum VinylGenre
+    {
+        AlternativeRock = 1,
+        Blues,
+        ClassicRock,
+        Pop,
+        Jazz,
+    }
+}
