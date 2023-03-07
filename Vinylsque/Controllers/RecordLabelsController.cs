18390171fa65bd894c2ef72bacee700e0ac5c59a
ivaskend <ivaskenderova98@gmail.com>
2023-03-07 20:27:07 +0100

    record label - 2 videos

--- Vinylsque/Controllers/RecordLabelsController.cs
+++ Vinylsque/Controllers/RecordLabelsController.cs
@@ -4,24 +4,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vinylesque.Data.Services;
 using Vinylsque.Data;
+using Vinylsque.Models;
 
 namespace Vinylsque.Controllers
 {
     public class RecordLabelsController : Controller 
-    { 
-       private readonly AppDbContext _context;
+    {
+        private readonly IRecordLabelsService _service;
 
-       public RecordLabelsController(AppDbContext context)
+       public RecordLabelsController(IRecordLabelsService service)
       {
-        _context = context;
+        _service = service;
 
       }
 
     public async Task<IActionResult> Index()
         {
-            var allRecordLabels = await _context.RecordLabels.ToListAsync();
+            var allRecordLabels = await _service.GetAllAsync();
             return View(allRecordLabels);
+        }
+        //GET: RecordLabel/Details/1
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var recordLabelDetails = await _service.GetByIdAsync(id);
+            if (recordLabelDetails == null) return View("NotFound");
+            return View(recordLabelDetails);
+        }
+
+        //GET: recordlabels/create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL, FullName, Bio")]RecordLabel recordLabel)
+        {
+            if (!ModelState.IsValid) return View(recordLabel);
+
+            await _service.AddAsync(recordLabel);
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ Vinylsque/Data/Services/RecordLabelsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vinylesque.Data.Base;
+using Vinylsque.Data;
+using Vinylsque.Models;
+
+namespace Vinylesque.Data.Services
+{
+    public class RecordLabelsService:EntityBaseRepository<RecordLabel>, IRecordLabelsService
+    {
+        public RecordLabelsService(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
